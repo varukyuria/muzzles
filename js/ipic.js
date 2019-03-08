1d5fb81d03135f589964c216654c3354be58a664
labrys <varukyuriachan@gmail.com>
2019-03-08 13:11:18 -0300

    a

--- js/ipic.js
+++ js/ipic.js
@@ -1,31 +1,60 @@
 export default class Ipic {
   constructor(config) {
+    this.config = config;
     this.name = config.name || '';
     this.selected = false;
+
     // how much should the mouse move before showing next frame
     this.mouseThreshold = config.mouseThreshold || 40;
+
+    // configure `frames` parameter
     this.currentFrame = 0;
     this.frames = config.frames;
     if (this.frames && !Array.isArray(this.frames)) {
       this.frames = [this.frames];
     }
+
+    // `fullview` is a pic that contains all the frames
     this.fullview = config.fullview;
+
+    // configure `days` parameter
     this.days = config.days || [];
     if (!Array.isArray(this.days)) {
       this.days = [this.days];
     }
+
+    // parent element
     this.parent = config.parent || document.body;
 
+    // create pics and stuff if today is in `days`
     if (this.days.length === 0 || this.days.includes((new Date()).getDay())) {
-      this.container = document.createElement('div');
-      this.container.classList.add('ipic-container');
-      this.parent.appendChild(this.container);
+      // create container
+      if (config.container) {
+        this.container = config.container;
+      }
+      else {
+        this.container = document.createElement('div');
+        this.container.classList.add('ipic-container');
+        this.parent.appendChild(this.container);
+      }
+
+      // add frame img
       if (this.frames) {
-        this.img = this.addImg(this.frames[0], this.container);
+        this.img = this.addImg(this.frames[0], this.container, config.width);
+        // set inline if in config
+        if (config.inline) {
+          this.img.style.display = 'inline';
+        }
+        // flip image
+        if (config.flip) {
+          this.img.style.transform = 'scaleX(-1)';
+        }
       }
+      // add fullview img
       if (this.fullview) {
-        this.imgFull = this.addImg(this.fullview, this.container);
+        this.imgFull = this.addImg(this.fullview, this.container, config.fullviewWidth);
       }
+      // warn if this ipic is basically useless
       if (!this.frames && !this.fullview) {
         console.warn(`${this.name === '' ? 'Unnamed Ipic' : `'${this.name}'`} doesn't have any images.`)
       }
@@ -42,7 +71,7 @@ export default class Ipic {
           e.preventDefault();
         };
 
-        // switch to frame based on mouse movement
+        // swap frames on mouse movement
         document.body.addEventListener('mousemove', (e) => {
           if (!this.selected) return;
           const delta = {x: e.pageX - initialMouse.x, y: e.pageY - initialMouse.y};
@@ -74,13 +103,14 @@ export default class Ipic {
     this.setFrame(this.currentFrame-1);
   }
 
-  addImg(src, parent) {
+  addImg(src, parent, maxWidth) {
     let ret = new Image();
     ret.src = src;
     ret.classList.add('ipic');
     if (parent) parent.appendChild(ret);
     ret.onload = () => {
-      ret.style.maxWidth = `${ret.naturalWidth}px`;
+      // set max image width to image's original width or to `maxWidth` if provided
+      ret.style.maxWidth = maxWidth ? maxWidth : `${ret.naturalWidth}px`;
     }
     return ret;
   }
--- js/main.js
+++ js/main.js
@@ -13,8 +13,50 @@ const main = document.getElementById('main');
 
 new Ipic({
   name: 'skull',
-  frames: generateFrames('skull00.jpg', 6, 'assets/pics/skull/frames'),
+  frames: generateFrames('skull00.jpg', 6, 'assets/pics/skull'),
   fullview: 'assets/pics/skull/skull.jpg',
+  parent: main,
+  fullviewWidth: '700px',
+  days: [],
+  mouseThreshold: -40
+});
+new Ipic({
+  name: 'skeleton',
+  frames: generateFrames('skeleton00.jpg', 3, 'assets/pics/skeleton'),
+  fullview: 'assets/pics/skeleton/skeleton.jpg',
+  parent: main,
+  days: [],
+  width: '250px',
+  fullviewWidth: '500px',
+  mouseThreshold: 40
+});
+let temp = new Ipic({
+  name: 'torso layers back',
+  frames: generateFrames('torso_layers_back00.jpg', 10, 'assets/pics/torso_layers/back'),
+  // fullview: 'assets/pics/torso_layers/back/torso_layers_back.jpg',
+  inline: true,
+  parent: main,
+  width: '300px',
+  days: [],
+  mouseThreshold: 20
+});
+new Ipic({
+  name: 'torso layers front',
+  frames: generateFrames('torso_layers_front00.jpg', 6, 'assets/pics/torso_layers/front'),
+  // fullview: 'assets/pics/torso_layers/front/torso_layers_front.jpg',
+  inline: true,
+  parent: main,
+  container: temp.container,
+  width: '300px',
+  days: [],
+  mouseThreshold: 20
+});
+
+// female external oblique
+new Ipic({
+  name: 'female external oblique',
+  frames: generateFrames('external_oblique00.jpg', 3, 'assets/pics/female/torso/external_oblique'),
+  // fullview: 'assets/pics/female/torso/external_oblique/external_oblique.jpg',
   parent: main,
   days: [],
   mouseThreshold: -40
